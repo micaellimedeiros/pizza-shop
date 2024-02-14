041d6bbcbdf505381a54df11ddf2354616744053
Micaelli Medeiros <m.amedeiros@hotmail.com>
2024-02-14 12:18:47 -0300

    Add new components and routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,9 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
+import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
@@ -14,6 +16,10 @@ export const router = createBrowserRouter([
         path: '/',
         element: <Dashboard />,
       },
+      {
+        path: '/orders',
+        element: <Orders />,
+      },
     ],
   },
   {
@@ -23,6 +29,10 @@ export const router = createBrowserRouter([
       {
         path: '/sign-in',
         element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
       },
     ],
   },
